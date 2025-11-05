8ed14ea01d85a726bfc427c3ff7b0affcc531cf7
Ronnakon <kamolkluaykaeo@gmail.com>
2025-11-06 00:45:45 +0700

    Q2

---
+++ lab_pointer2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int num,sum=0;
+    scanf("%d",&num);
+    int array[num];
+    for(int i=0; i<num; i++){
+        scanf("%d",&array[i]);
+    }
+    for(int i=0; i<num; i++){
+        printf("%p\n",&array[i]);
+    }
+     for(int i=0; i<num; i++){
+        int*par = &array[i];
+        sum += *par;
+    }
+    printf("Sum = %d",sum);
+    
+    return 0;
+}
