cc102b2b097be9659c96dfc665ab1cd94c27f707
Ronnakon <kamolkluaykaeo@gmail.com>
2025-11-05 23:55:33 +0700

    Q1

--- lab_pointer.c
+++ lab_pointer.c
@@ -11,7 +11,7 @@ int main(){
     printf("char = %c at %p\n",*pname,&Char);
     printf("int = %d at %p\n",*pint,&Number);
     printf("float = %.2f at %p\n",*pnumber,&Float);
-    
+
     return 0;
 }
 
